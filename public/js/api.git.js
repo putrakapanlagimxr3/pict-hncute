f1092f5f299ca776bb0c897aba0888aeb9f11bbf
putrakapanlagimxr3 <putrakapanlagi@gmail.com>
2026-03-14 09:36:38 +0700

    install dependencies

--- public/js/api.git.js
+++ public/js/api.git.js
@@ -1,10 +1,6 @@
 const githubConfig = {
     token: "", 
-<<<<<<< HEAD
-    owner: "your-github-username", // GANTI dengan username github lo
-=======
     owner: "putrakapanlagimxr3", // GANTI dengan username github lo
->>>>>>> 377b2e70430a861112be70a81d3c77b1069c06e0
     repo: "pict-hncute",
     path: "hncute_files",
     adminAvatarPath: "image-admin",
@@ -23,8 +19,4 @@ window.ghRequest = async function(path, options = {}) {
             ...options.headers
         }
     });
-<<<<<<< HEAD
 };
-=======
-};
->>>>>>> 377b2e70430a861112be70a81d3c77b1069c06e0
